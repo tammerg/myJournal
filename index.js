b202275326f30a3b3a3f9fab1fca8138b05a52d6
tammerg <tg385@scarletmail.rutgers.edu>
2016-02-11 21:07:54 -0500

    made some small chanhges to my note keeper!

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ var express = require('express');
 var expressHandlebar = require('express-handlebars');
 var session = require ('session');
 var mysql = require('mysql');
+var bodyParser = require('body-parser');
 var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 var PORT = process.env.NODE_EV || 8080
 var connection = mysql.createConnection({
   port : 3306,
@@ -14,12 +16,19 @@ var connection = mysql.createConnection({
 app.engine('handlebars', expressHandlebar({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-app.listen(PORT, function(){
-  console.log('you are listening on port %s', PORT);
-});
 app.get('/', function(req, res){
   connection.query('SELECT * FROM quick_notes', function(err, data){
     if (err) throw err;
     res.render('notes', {data});
   });
+});
+app.post('/create', function(req, res){
+  connection.query('INSERT INTO quick_notes (note) VALUES (?)', [req.body.note], function(){
+    if (err) throw err;
+  });
+    res.redirect('/');
+})
+
+app.listen(PORT, function(){
+  console.log('you are listening on port %s', PORT);
 });
